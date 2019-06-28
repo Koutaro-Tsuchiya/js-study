edf1be028afe71fb2b2b458e15085ea9bb5198fb
Koutaro-Tsuchiya <gataca.gene@gmail.com>
2019-06-28 21:59:13 +0900

    修正

--- chapter7-4-q2/js/index.js
+++ chapter7-4-q2/js/index.js
@@ -3,75 +3,92 @@ let tmp = 0;  //前回の計算を取得
 let key = "";　//演算子の取得
 let keyTmp = "";　//１つ前の演算子の取得
 let display = document.querySelector('.display');
+let number = document.querySelectorAll('.number');
+let operator = document.querySelectorAll('.operator')
+const clear = document.querySelector('.clear')
+const sign = document.querySelector('.sign')
+const percent = document.querySelector('.percent')
 
-// 「数字」「.」を押した場合
-function calcInput(btn) {
-    // ここの挙動が不明
-    if (btn == '.') {
-        if (display.innerText.match(new RegExp("\\.")) != null) return;
+// 「数字」「.」が押された場合
+number.forEach(index => {
+  index.addEventListener('click', () => {
+    if(index.innerText == "."){
+      if(display.innerText.match(new RegExp("\\.")) != null){
+      return;
+      }
+      if(display.innerText == ""){
+        display.innerText = 0;
+      }
     }
     //演算子が既に入力されていた場合
     if (keyTmp != "")displayClear();
     //入力された数字を表示
     //1回目の数字の入力はここに飛ぶ
-    display.innerText = display.innerText + btn;
-}
+    display.innerText += index.innerText;
+  })
+})
 
-//数字の入力が行われた時に演算子が既に入力されているタイミングで表示画面の値をクリア
 function displayClear() {
-  document.querySelector('.display').innerText = "";
+  display.innerText = "";
   keyTmp = "";
 }
 
 //演算子が押された場合
-function calcRun(keyInp) {
+operator.forEach(index => {
+  index.addEventListener('click', () => {
     //tmpに数字を保存
-    tmp = display.innerHTML;
+    tmp = display.innerText;
     //keyに演算子が入っている場合
     if (key != "") {
         ans = calcAny(ans, tmp, key);
         tmp = 0;
-        display.innerText = ans * 1;
+        display.innerText = ans;
     }
     // 「＝」を押した場合
-    if (keyInp == '='){
+    if (index.innerText == '='){
       key = "";
       keyTmp = "";
       ans = "";
-    }
+      }
     // １回目の演算子の入力はここに飛ぶ
     // keyに前回押した演算子を保存
     else {
-      key = keyInp;
-      keyTmp = keyInp;
+      key = index.innerText;
+      keyTmp = index.innerText;
       //計算結果
-      ans += tmp * 1;
+      ans += tmp;
     }
-}
+  })
+})
 
 function calcAny(ans, tmp, key) {
-  if (key == '+') { return ans * 1 + tmp * 1 }
-  if (key == '-') { return ans * 1 - tmp * 1 }
-  if (key == '×') { return ans * 1 * tmp * 1 }
-  if (key == '÷') { return ans * 1 / tmp * 1 }
+  if (key == '+') { return parseInt(ans) + parseInt(tmp) }
+  if (key == '-') { return parseInt(ans)  - parseInt(tmp)}
+  if (key == '×') { return parseInt(ans) * parseInt(tmp) }
+  if (key == '÷') { return parseInt(ans)  / parseInt(tmp) }
 }
 
 // ACを押した場合
-function calcInit() {
-  document.querySelector('.display').innerText = "";
+clear.addEventListener('click', () =>{
+  display.innerText = "";
   ans = 0;
   tmp = 0;
   key = "";
+})
+
+class calc{
+    static calcSpecial(x){
+    display.innerText = display.innerText * x;
+    tmp = display.innerText;
+  }
 }
 
 // +/-を押した場合
-function calcNegated() {
-    display.innerText = display.innerHTML * -1;
-    tmp = display.innerHTML;
-}
+sign.addEventListener('click', () =>{
+  calc.calcSpecial(-1);
+})
 
 // %を押した場合
-function calcPercent() {
-    display.innerText = display.innerHTML * 0.01;
-    tmp = display.innerHTML;
-}
+percent.addEventListener('click', () =>{
+  calc.calcSpecial(0.01);
+})

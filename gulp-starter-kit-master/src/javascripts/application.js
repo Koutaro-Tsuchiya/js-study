24dc9a5b43b70cb03ab2ca6061d449ce06c95907
Koutaro-Tsuchiya <gataca.gene@gmail.com>
2019-07-15 20:42:38 +0900

    修正

--- gulp-starter-kit-master/src/javascripts/application.js
+++ gulp-starter-kit-master/src/javascripts/application.js
@@ -1,1 +1,14 @@
-console.log("Application")
+const toggleBtn = document.querySelector('.toggle-btn');
+let linkMenu = document.querySelectorAll('.link-menu');
+
+linkMenu.forEach(element => {
+  toggleBtn.addEventListener('click', ()=>{
+    if(element.classList.contains('open')) {
+      element.classList.remove('open');
+      element.classList.add('close');
+    } else {
+      element.classList.remove('close');
+      element.classList.add('open');
+    }
+  })
+})
